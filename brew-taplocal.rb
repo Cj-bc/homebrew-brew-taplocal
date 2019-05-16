1ed0997ebfa7fa9e54edd6c0cfea042f43110cde
Cj-bc <cj-bc@y-modify.org>
2019-05-16 15:34:50 +0900

    Add: minimum formula

---
+++ brew-taplocal.rb
@@ -0,0 +1,10 @@
+class BrewTaplocal < Formula
+  desc "tap local formula repository"
+  homepage "https://github.com/Cj-bc/brew-taplocal"
+  url "https://github.com/Cj-bc/brew-taplocal.git"
+
+
+  def install
+    bin.install brew-taplocal
+  end
+end
