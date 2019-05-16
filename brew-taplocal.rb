14a0733e2eddf8ff9024575d929a5e9a85277211
Cj-bc <cj-bc@y-modify.org>
2019-05-16 16:17:58 +0900

    Fix: url

--- brew-taplocal.rb
+++ brew-taplocal.rb
@@ -1,8 +1,7 @@
 class BrewTaplocal < Formula
   desc "tap local formula repository"
   homepage "https://github.com/Cj-bc/brew-taplocal"
-  url "https://github.com/Cj-bc/brew-taplocal.git"
-
+  url "https://github.com/Cj-bc/brew-taplocal/archive/v0.1.0.tar.gz"
 
   def install
     bin.install brew-taplocal
