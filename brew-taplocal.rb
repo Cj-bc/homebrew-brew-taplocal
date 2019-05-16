2dab09618f475353b0b0efa98853a7c822bf3173
Cj-bc <cj-bc@y-modify.org>
2019-05-16 18:35:56 +0900

    Update 0.2.0

--- brew-taplocal.rb
+++ brew-taplocal.rb
@@ -1,9 +1,9 @@
 class BrewTaplocal < Formula
   desc "tap local formula repository"
   homepage "https://github.com/Cj-bc/brew-taplocal"
-  url "https://github.com/Cj-bc/brew-taplocal/archive/v0.1.0.tar.gz"
+  url "https://github.com/Cj-bc/brew-taplocal/archive/v0.2.0.tar.gz"
   head "https://github.com/Cj-bc/brew-taplocal.git"
-  sha256 "d6e04590bc6b8829749227f15447df38db62699b06da6f426d47ec031a937a3c"
+  sha256 "b1792909dde908e0978aac9b8cc0d60410ad12512f478317f5ad5c1030f7f3ad"
 
   def install
     bin.install brew-taplocal
