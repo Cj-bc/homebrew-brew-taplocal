23cfe29ee4daa67913d8245f0c005c0828c31e9e
Cj-bc <cj-bc@y-modify.org>
2019-05-16 16:30:36 +0900

    Add: head, sha256

--- brew-taplocal.rb
+++ brew-taplocal.rb
@@ -2,6 +2,8 @@ class BrewTaplocal < Formula
   desc "tap local formula repository"
   homepage "https://github.com/Cj-bc/brew-taplocal"
   url "https://github.com/Cj-bc/brew-taplocal/archive/v0.1.0.tar.gz"
+  head "https://github.com/Cj-bc/brew-taplocal.git"
+  sha256 "d6e04590bc6b8829749227f15447df38db62699b06da6f426d47ec031a937a3c"
 
   def install
     bin.install brew-taplocal
